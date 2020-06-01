e767ee5051a46489015b345a24523bcd55ebcbd6
Mohammad Raska <iban.ninja@gmail.com>
2020-06-01 20:28:34 +0700

    handle 2 step back disabled button next

--- src/components/FormWizard.vue
+++ src/components/FormWizard.vue
@@ -2,6 +2,7 @@
   <div>
     <keep-alive>
       <component 
+        ref="currentStep"
         :is="currentStep" 
         @update="processStep" 
         :wizard-data="form">
@@ -88,7 +89,10 @@ export default {
     },
     goNext () {
       this.currentStepNumber++
-      this.canGoNext = false
+      // this.canGoNext = false
+      this.$nextTick(() =>{
+        this.canGoNext = !this.$refs.currentStep.$v.$invalid
+      })
     }
   }
 }
